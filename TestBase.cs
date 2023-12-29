d52d49d2bab45f1faf429a8bb3a6595b0b6fdfb9
Oh-Nahyun <101961487+Oh-Nahyun@users.noreply.github.com>
2023-12-29 15:08:53 +0900

    Add files via upload

---
+++ TestBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start 함수는 첫번째 프레임의 Update가 실행되기 직전에 호출된다. (시작할 때 호출한다.)
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    // Update 함수는 프레임마다 한번씩 호출된다.
+    void Update()
+    {
+        
+    }
+}
